90d200cb4718863470d9cf37ce036eb9964b94cd
alicantarim <alicantarim@hotmail.com>
2024-12-19 09:16:33 +0300

    Initial Commit

---
+++ LearnConnectTests/LearnConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnConnectTests.swift
+//  LearnConnectTests
+//
+//  Created by Alican TARIM on 19.12.2024.
+//
+
+import Testing
+@testable import LearnConnect
+
+struct LearnConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
